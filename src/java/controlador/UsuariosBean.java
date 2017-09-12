919c52c2be69355d7de4354d93021ca07300dd3e
medev <medev@instructor10-OptiPlex-7010>
2017-09-12 12:56:16 -0600

    cambios

--- src/java/controlador/UsuariosBean.java
+++ src/java/controlador/UsuariosBean.java
@@ -1,41 +1,72 @@
-
 package controlador;
 
-
+import dao.UsuarioDAO;
+import java.io.Serializable;
 
 import java.util.List;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+
 import modelo.Usuarios;
 
 @ManagedBean
 @ViewScoped
-public class UsuariosBean {
-    private Usuarios empleado = new Usuarios();
-    private List<Usuarios> Usuarios;
-    
-    public UsuariosBean() {
+public class UsuariosBean implements Serializable{
+
+  private Usuarios usuario = new Usuarios();
+  private Usuarios usuarioSesion = new Usuarios();
+  private List<Usuarios> lstUsuarios;
+
+  public UsuariosBean() {
+
+  }
+
+  public Usuarios getUsuario() {
+    return usuario;
+  }
+
+  public void setUsuario(Usuarios usuario) {
+    this.usuario = usuario;
+  }
+
+  public List<Usuarios> getLstUsuarios() {
+    return lstUsuarios;
+  }
+
+  public void setLstUsuarios(List<Usuarios> lstUsuarios) {
+    this.lstUsuarios = lstUsuarios;
+  }
+
+  public void listar() throws Exception {
+
+    UsuarioDAO dao = new UsuarioDAO();
+
+    try {
+     usuarioSesion = dao.usuariolistar(usuario);
+
+      //if (usuario.getContraseña().equals(usuarioSesion.getContraseña())) {
+        if (usuarioSesion!=null){
         
+        
+        FacesContext context = FacesContext.getCurrentInstance();
+        
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("sesion", usuarioSesion);
+        
+        
+        
+        
+        
+        context.getExternalContext().redirect("faces/nueva.xhtml");
+        System.out.println("llega?");
+        System.out.println("Se creo todo correctamente");
+      }
+    } catch (Exception e) {
+      System.out.println("se encontro un error en el Usuario BEAN");
+      System.out.println("Error en crear sesion UsuarioBean : " + e);
     }
 
-    public Usuarios getEmpleado() {
-        return empleado;
-    }
+  }
 
-    public void setEmpleado(Usuarios empleado) {
-        this.empleado = empleado;
-    }
-
-    public List<Usuarios> getUsuarios() {
-        return Usuarios;
-    }
-
-    public void setUsuarios(List<Usuarios> Usuarios) {
-        this.Usuarios = Usuarios;
-    }
-    
-
-    
-   
-    
 }
